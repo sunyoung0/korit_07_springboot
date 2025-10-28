0ba577713a88002b2a515865459bf3871ffa124c
sunyoung0 <sunyp0713@naver.com>
2025-10-28 16:42:17 +0900

    feat : 20251028 git ignore setting

--- shoppinglist2/src/main/java/com/shoppinglist/shoppinglist2/web/LoginController.java
+++ shoppinglist2/src/main/java/com/shoppinglist/shoppinglist2/web/LoginController.java
@@ -31,7 +31,7 @@ public class LoginController {
 
         // JWT를 'Authorization' 헤더에 담아서 응답
         return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer ", jwt)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwt)
                 .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization") // FrontEnd에서 헤더를 읽을 수 있도록 설정
                 .build();
     }
